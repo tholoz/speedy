e55638dae83808733235abe61a4e7c8c5175c274
Pierre <pierre.tholoniat@polytechnique.edu>
2018-02-28 12:35:21 +0100

    boolean Checker.check(String)

---
+++ src/main/java/speedy/Checker.java
@@ -0,0 +1,29 @@
+package speedy;
+
+import org.json.JSONObject;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+
+public class Checker {
+	public static boolean check(String word) {
+		try {
+			
+		HttpResponse<JsonNode> response = Unirest.get("https://montanaflynn-spellcheck.p.mashape.com/check/?text="+word)
+		.header("X-Mashape-Key", "pZfvRi4Qodmshkq5DvbkEGACNLZvp1rwasCjsnEQRySCqOuQn0")
+		.header("Accept", "application/json")
+		.asJson();
+		
+		JSONObject jsobj = response.getBody().getObject();
+		JSONObject corr = jsobj.getJSONObject("corrections");
+		
+		return(corr.toString().length()==2);
+		}catch (Exception e) {
+			e.printStackTrace();
+			return(false);
+		}
+	
+	
+	}
+}
